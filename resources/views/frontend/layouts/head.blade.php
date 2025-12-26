f5bde2b1a4c32e38da84b9d4489c2f7e68402163
tt1989182-cyber <tt1989182@gmail.com>
2025-12-26 17:57:58 +0700

    Update head.blade.php

--- resources/views/frontend/layouts/head.blade.php
+++ resources/views/frontend/layouts/head.blade.php
@@ -1,28 +1,28 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-@yield('meta')
 <title>@yield('title','Trang chủ')</title>
 
-<link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
+<base href="https://aurashop.up.railway.app/">
+
+<link rel="icon" type="image/png" href="images/favicon.png">
 
 <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&display=swap" rel="stylesheet">
 
-<!-- CORE CSS -->
-<link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/font-awesome.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/themify-icons.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/magnific-popup.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/jquery.fancybox.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/niceselect.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/animate.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/flex-slider.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/owl-carousel.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/slicknav.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/jquery-ui.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/reset.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/responsive.css') }}">
-
-<!-- ICON CỨU NGAY -->
+<!-- ICON -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/themify-icons/0.1.2/css/themify-icons.css">
+
+<!-- CSS -->
+<link rel="stylesheet" href="frontend/css/bootstrap.css">
+<link rel="stylesheet" href="frontend/css/font-awesome.css">
+<link rel="stylesheet" href="frontend/css/magnific-popup.min.css">
+<link rel="stylesheet" href="frontend/css/jquery.fancybox.min.css">
+<link rel="stylesheet" href="frontend/css/niceselect.css">
+<link rel="stylesheet" href="frontend/css/animate.css">
+<link rel="stylesheet" href="frontend/css/flex-slider.min.css">
+<link rel="stylesheet" href="frontend/css/owl-carousel.css">
+<link rel="stylesheet" href="frontend/css/slicknav.min.css">
+<link rel="stylesheet" href="frontend/css/jquery-ui.css">
+<link rel="stylesheet" href="frontend/css/reset.css">
+<link rel="stylesheet" href="frontend/css/style.css">
+<link rel="stylesheet" href="frontend/css/responsive.css">
