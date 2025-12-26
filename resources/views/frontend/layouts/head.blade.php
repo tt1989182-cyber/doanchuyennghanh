636a0f00ae1417d6898925c7200abb0870a0328e
tt1989182-cyber <tt1989182@gmail.com>
2025-12-26 17:08:53 +0700

    Update head.blade.php

--- resources/views/frontend/layouts/head.blade.php
+++ resources/views/frontend/layouts/head.blade.php
@@ -1,41 +1,17 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
 @yield('meta')
 <title>@yield('title','Trang chủ')</title>
 
 <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
 
-<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&display=swap" rel="stylesheet">
 
-<!-- ===== CSS ===== -->
+<!-- CORE CSS -->
 <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.css') }}">
 <link rel="stylesheet" href="{{ asset('frontend/css/font-awesome.css') }}">
 <link rel="stylesheet" href="{{ asset('frontend/css/themify-icons.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/magnific-popup.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/jquery.fancybox.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/niceselect.css') }}">
 <link rel="stylesheet" href="{{ asset('frontend/css/animate.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/flex-slider.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/owl-carousel.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/slicknav.min.css') }}">
-<link rel="stylesheet" href="{{ asset('frontend/css/jquery-ui.css') }}">
 
-<link rel="stylesheet" href="{{ asset('frontend/css/reset.css') }}">
 <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
 <link rel="stylesheet" href="{{ asset('frontend/css/responsive.css') }}">
-
-<style>
-.dropdown-submenu {
-    position: relative;
-}
-.dropdown-submenu>a:after {
-    content: "\f0da";
-    float: right;
-    font-family: 'FontAwesome';
-}
-.dropdown-submenu>.dropdown-menu {
-    top: 0;
-    left: 100%;
-}
-</style>
