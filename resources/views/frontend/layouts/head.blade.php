726f387193002168fc7887b736f19ff5ff02c114
tt1989182-cyber <tt1989182@gmail.com>
2025-12-26 17:34:03 +0700

    Update head.blade.php

--- resources/views/frontend/layouts/head.blade.php
+++ resources/views/frontend/layouts/head.blade.php
@@ -8,18 +8,17 @@
 <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&display=swap" rel="stylesheet">
 
 <!-- CORE CSS -->
-<link rel="stylesheet" href="{{ secure_asset('frontend/css/bootstrap.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('frontend/css/font-awesome.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('frontend/css/themify-icons.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('frontend/css/magnific-popup.min.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('frontend/css/jquery.fancybox.min.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('frontend/css/niceselect.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('frontend/css/animate.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('frontend/css/flex-slider.min.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('frontend/css/owl-carousel.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('frontend/css/slicknav.min.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('frontend/css/jquery-ui.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('frontend/css/reset.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('frontend/css/style.css') }}">
-<link rel="stylesheet" href="{{ secure_asset('frontend/css/responsive.css') }}">
-
+<link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/font-awesome.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/themify-icons.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/magnific-popup.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/jquery.fancybox.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/niceselect.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/animate.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/flex-slider.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/owl-carousel.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/slicknav.min.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/jquery-ui.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/reset.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('frontend/css/responsive.css') }}">
